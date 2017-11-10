6a50e14ad446f591348dc355de9381ab74210382
Joe Engalan <joe.engalan@gmail.com>
2017-11-10 12:06:34 -0500

    NCDK-00 Change resource loader to a Unity-specific loader.

--- NCDK/ResourceLoader.cs
+++ NCDK/ResourceLoader.cs
@@ -5,18 +5,42 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
+using NCDK.Layout;
+using UnityEngine;
 
 namespace NCDK
 {
     public static class ResourceLoader
     {
-        public static Stream GetAsStream(string name)
+        private static Stream GetAsUnityStream(string name)
         {
+            {
+                var fileExt = Path.GetExtension(name);
+                var pathComponents = name.Split('.');
+                var fileName = pathComponents[pathComponents.Length - 2];
+
+                var fullPath = Path.Combine(Application.dataPath, "NCDK");
+                if (pathComponents.Length > 2)
+                {
+                    for (int i = 0; i < pathComponents.Length - 2; i++)
+                    {
+                        fullPath = Path.Combine(fullPath, pathComponents[i]);
+                    }
+                }
+
+                fullPath = Path.Combine(fullPath, fileName + fileExt);
+
+                UnityEngine.Debug.Log("Loading data file from: " + fullPath);
+
+                name = fullPath;
+            }
+
             if (File.Exists(name))
             {
                 try
                 {
                     var srm = new FileStream(name, FileMode.Open, FileAccess.Read, FileShare.Read);
+                    UnityEngine.Debug.Log("Loaded stream from " + name);
                     return srm;
                 }
                 catch (Exception exception)
@@ -25,46 +49,25 @@ namespace NCDK
                 }
             }
 
-            {
-                var asm = Assembly.GetCallingAssembly();
-                var srm = asm.GetManifestResourceStream(name);
-                if (srm != null)
-                    return srm;
-            }
-
-            foreach (var asm in AppDomain.CurrentDomain.GetAssemblies())
-            {
-                try
-                {
-                    var srm = asm.GetManifestResourceStream(name);
-                    if (srm != null)
-                        return srm;
-                }
-                catch (Exception)
-                {
-                    // ignore
-                }
-            }
-
             Trace.TraceInformation($"Resource not found {name}.");
             return null;
-        }
 
-        public static Stream GetAsStream(Assembly asm, string name)
+        }
+        public static Stream GetAsStream(string name)
         {
-            var srm = asm.GetManifestResourceStream(name);
-            if (srm == null)
-                Trace.TraceInformation($"Resource not found {name}.");
-            return srm;
+            return GetAsUnityStream(name);
         }
 
         public static Stream GetAsStream(Type type, string name)
         {
-            var asm = type.Assembly;
-            var srm = asm.GetManifestResourceStream(type, name);
-            if (srm == null)
-                Trace.TraceInformation($"Resource not found {name}.");
-            return srm;
+            if(type == typeof(IdentityTemplateLibrary))
+            {
+                string relativePath = "NCDK/Layout/" + name;
+                return GetAsUnityStream(relativePath);
+            }
+
+            Trace.TraceInformation($"Resource not found {name}.");
+            return null;
         }
     }
 }
